28a01b4576548d19f28a5266045f3c5525505b56
Madhushani Hewagama <madhushanihewagama.16@cse.mrt.ac.lk>
2021-02-01 16:07:19 +0530

    remove caption buttoin

--- src/Captions.js
+++ src/Captions.js
@@ -1,5 +1,5 @@
 import React from "react";
-import {captionData} from './data';
+// import {captionData} from './data';
 import '../node_modules/video-react/dist/video-react.css'
 // import "node_modules/video-react/dist/video-react.css";
 import { Player, ControlBar, ClosedCaptionButton } from 'video-react';
@@ -7,9 +7,6 @@ import MIB2 from './MIB2.mp4'
 import sub from './sub.en.vtt'
 export const Captions = () => {
     return (
-        // <div className="caption-container">
-    
-
       <div className="caption-container">
       <Player videoId="video-1" autoPlay>
       <source
@@ -19,36 +16,11 @@ export const Captions = () => {
       <track
         kind="captions"
         src={sub}
-        srcLang="en"
-        label="English"
+        // srcLang="en"
+        // label="English"
         default
       />
-      <ControlBar autoHide={false}>
-        <ClosedCaptionButton order={1} />
-      </ControlBar>
       </Player>
-    {/* this.player.addTextTrack({
-        src: './sub.vtt',
-        kind: 'subtitles',
-        srclang: 'en',
-        label: 'English'
-      }) */}
-        {/* {captionData.map((data, key) => {
-          return (
-            <div key={key}>
-              {data.text +
-                " , " +
-                data.startTime +
-                " ," +
-                data.endTime }
-            </div>
-          );
-        })} */}
-        
       </div>
-
-
-    
-        // </div>
     );
 }
